3f34026817ac3e35724d1b77efd72b412419b6c7
root <snowdbat@gmail.com>
2024-09-26 12:24:27 -0700

    Handle arguments

--- execute.c
+++ execute.c
@@ -12,13 +12,25 @@ void execute(const char *command)
 
 	if (ch_pid == -1)
 	{
-		perror("fork");
+		shprint("Forking error");
 		exit(EXIT_FAILURE);
 	}
 	else if (ch_pid == 0)
 	{
-		execlp(command, command, (char *) NULL);
-		perror("execlp");
+		char *args[200];
+		int arg_count = 0;
+		char *tocken = strtok((char *)command, " ");
+
+		while (tocken != NULL)
+		{
+			args[arg_count++] = tocken;
+			tocken = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
+
+		execvp(args[0], args);
+
+		shprint("Execution error.\n");
 		exit(EXIT_FAILURE);
 	}
 	else
