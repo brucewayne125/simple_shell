0846842aa8d648173257e55482651d2c576a7ef4
root <snowdbat@gmail.com>
2024-09-27 11:35:00 -0700

    shellbasic

--- execute_command.c
+++ execute_command.c
@@ -1,45 +1,49 @@
-#include "shell.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
 
-void execute_command(char *command)
-{
+#define BUFFER_SIZE 1024
+
+void execute_command(char *command) {
 	pid_t pid = fork();
 
-	if (pid == 0)
-	{
-		char *args[BUFFER_SIZE];
-		char *token;
-		int index = 0;
-		
-		token = strtok(command, " ");
-		while (token != NULL)
-		{
-			args[index++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[index] = NULL;
-		
-		if (execve(args[0], args, NULL) == -1)
-		{
-			char error_msg[BUFFER_SIZE];
-			snprintf(error_msg, sizeof(error_msg), "./hsh: 1: %s: not found\n", args[0]);
-			write(STDERR_FILENO, error_msg, strlen(error_msg));
-		}
-		exit(EXIT_FAILURE);
+	if (pid < 0) {
+		perror("Fork failed");
+		return;
 	}
-	else if (pid < 0)
-	{
-		const char *fork_error = "fork failed\n";
-		
-		write(STDERR_FILENO, fork_error, strlen(fork_error));
-	}
-	else
-	{
+
+	if (pid == 0) {
+		if (execlp(command, command, (char *)NULL) == -1) {
+			perror(command);
+			exit(EXIT_FAILURE);
+		}
+	} else {
 		wait(NULL);
 	}
 }
+
+int main() {
+	char command[BUFFER_SIZE];
+
+	while (1) {
+		printf("#cisfun$ ");
+		if (fgets(command, BUFFER_SIZE, stdin) == NULL) {
+			printf("\n");
+			break;
+		}
+
+		command[strcspn(command, "\n")] = 0;
+
+		if (strlen(command) == 0) {
+			continue;
+		}
+
+		execute_command(command);
+	}
+
+	return 0;
+}
+
