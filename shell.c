0bd56d05a3ca47d1237f45c6f0aa7d2a6563fe55
root <snowdbat@gmail.com>
2024-09-27 08:12:17 -0700

    interpreter

--- shell.c
+++ shell.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 1024
 
 void prompt() {
-    printf("($) ");
+    const char *prompt_str = "($) ";
+    write(STDOUT_FILENO, prompt_str, strlen(prompt_str));
 }
 
 void execute_command(char *command) {
     pid_t pid = fork();
     if (pid == 0) {
         char *args[2];
-	args[0] = command;
-	args[1] = NULL;
+        args[0] = command;
+        args[1] = NULL;
         if (execve(command, args, NULL) == -1) {
-            perror("./shell");
+            const char *error_msg = "./shell: No such file or directory\n";
+            write(STDERR_FILENO, error_msg, strlen(error_msg));
         }
         exit(EXIT_FAILURE);
     } else if (pid < 0) {
-        perror("fork");
+        const char *fork_error = "fork failed\n";
+        write(STDERR_FILENO, fork_error, strlen(fork_error));
     } else {
         wait(NULL);
     }
@@ -31,14 +34,15 @@ void execute_command(char *command) {
 int main() {
     char *command = malloc(BUFFER_SIZE);
     if (!command) {
-        perror("malloc");
+        const char *malloc_error = "malloc failed\n";
+        write(STDERR_FILENO, malloc_error, strlen(malloc_error));
         exit(EXIT_FAILURE);
     }
 
     while (1) {
         prompt();
         if (fgets(command, BUFFER_SIZE, stdin) == NULL) {
-            printf("\n");
+            write(STDOUT_FILENO, "\n", 1);
             free(command);
             exit(EXIT_SUCCESS);
         }
